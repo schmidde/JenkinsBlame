c1ea94fc97e4393e6314a842597373832d5e79f5
Dennis Schmidt <schmide@fh-brandenburg.de>
2011-12-11 00:50:54 +0100

    initial commit

---
+++ src/de/fhb/sq/JenkinsBlameServlet.java
@@ -0,0 +1,13 @@
+package de.fhb.sq;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class JenkinsBlameServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
