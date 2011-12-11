9d8d099bbdc4a6880a44cc15697e37b0e5c17062
Dennis Schmidt <schmide@fh-brandenburg.de>
2011-12-11 21:05:15 +0100

    json-rpc.jar hinzu

--- src/de/fhb/sq/JenkinsDataCaller.java
+++ src/de/fhb/sq/JenkinsDataCaller.java
@@ -1,5 +1,33 @@
 package de.fhb.sq;
 
-public class JenkinsDataCaller {
+import java.util.Date;
+import org.json.JSONObject;
 
+/**
+ * Die Klasse holt von einem Jenkins CI Server XML oder JSON. 
+ * Der Pfad des Servers und der Name des Projekts muss angegeben werden
+ * 
+ * @author Dennis Schmidt
+ * @author Sebastian Gräbitz
+ * 
+ * @version 0.1
+ * */
+public class JenkinsDataCaller {
+	
+	private int buildNr;
+	private String buildPath, buildJob, buildFarbe;
+	private Date buildDate;
+	
+	public JenkinsDataCaller(String buildPath, String buildJob){
+		this.buildPath = buildPath;
+		this.buildJob = buildJob;
+	}
+	
+	public void callXml(){
+		//Todo
+	}
+
+	public void callJson(){
+		//Todo
+	}
 }
