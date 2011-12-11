b500e4065a854661aa126a00219eb734365592eb
Dennis Schmidt <schmide@fh-brandenburg.de>
2011-12-11 23:44:11 +0100

    package org.json hinzu

--- src/de/fhb/sq/JenkinsDataCaller.java
+++ src/de/fhb/sq/JenkinsDataCaller.java
@@ -7,7 +7,8 @@ import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
 import java.nio.charset.Charset;
-
+import org.json.JSONObject;
+import org.json.JSONException;
 import java.util.Date;
 
 /**
