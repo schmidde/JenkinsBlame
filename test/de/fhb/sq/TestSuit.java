c593717df1de43e322f6e4dc185967aa3e9f58ea
Dennis Schmidt <schmidde@fh-brandenburg.de>
2011-12-24 12:20:00 +0100

    Test-Target zum build.xml hinzu

---
+++ test/de/fhb/sq/TestSuit.java
@@ -0,0 +1,13 @@
+package de.fhb.sq;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+  JenkinsJsonParserTest.class,
+  
+})
+
+public class TestSuit {
+
+}
