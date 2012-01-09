9dbff02e5ffef83dd9f38058ce29bf99ddd51735
Dennis Schmidt <schmidde@fh-brandenburg.de>
2012-01-09 21:08:33 +0100

    einige Integrationstests

--- integrations-tests/de/fhb/sq/JenkinsBlameStatsServletIntegrationTest.java
+++ integrations-tests/de/fhb/sq/JenkinsBlameStatsServletIntegrationTest.java
@@ -26,19 +26,11 @@ public class JenkinsBlameStatsServletIntegrationTest {
 	private static JenkinsBlameStatsServlet jbs;
     private final LocalServiceTestHelper helper =
         new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
-
-    @BeforeClass
-    public static void setUpBeforeClass(){
-    	jbs = new JenkinsBlameStatsServlet("http://jenkins.rambow.it:8080", "JenkinsBlame");
-    }
-    @Before
-    public void setUp() {
-        helper.setUp();
-        Build b1, b2;
-        b1 = new Build(1234567, 1, "blue", "Dennis Schmidt");
-        b2 = new Build(1234568, 2, "red", "Dennis Schmidt");
+    
+    public void initDS(String projectName, Build b1, Build b2){
+    
         List<Build> bl = new ArrayList();
-        Project p = new Project(jbs.getJobName());
+        Project p = new Project(projectName);
         bl.add(b1); bl.add(b2);
         p.setBuilds(bl);
         p.setLastFailedBuild(2);
@@ -55,17 +47,28 @@ public class JenkinsBlameStatsServletIntegrationTest {
         	}
             pm.close();
         }
-
+    }
+    @BeforeClass
+    public static void setUpBeforeClass(){
+    	jbs = new JenkinsBlameStatsServlet("http://jenkins.rambow.it:8080", "JenkinsBlame");
+    }
+    @Before
+    public void setUp() {
+        helper.setUp();    
     }
 
     @After
     public void tearDown() {
         helper.tearDown();
     }
-
+    
     @Test
     public void getBuildsByNameTest() {
-   
+    	Build b1 = new Build(1234567, 1, "blue", "Dennis Schmidt");
+    	Build b2 = new Build(1234568, 2, "red", "Dennis Schmidt");
+    	
+    	initDS(jbs.getJobName(), b1, b2);
+    	
     	List<Build> builds = null;
     	builds = jbs.getBuildsByName(jbs.getJobName());
     	assertTrue(!builds.isEmpty());
@@ -75,9 +78,61 @@ public class JenkinsBlameStatsServletIntegrationTest {
     @Test
     public void hasJobTest() {
     	
+    	Build b1 = new Build(1234567, 1, "blue", "Dennis Schmidt");
+    	Build b2 = new Build(1234568, 2, "red", "Dennis Schmidt");
+    	
+    	initDS(jbs.getJobName(), b1, b2);
+    	
     	assertTrue(jbs.hasJob(jbs.getJobName()));
     	assertFalse(jbs.hasJob(""));
     }
-
+    @Test
+    public void isNewTest(){
+    	
+    	Build b1 = new Build(1234567, 1, "blue", "Dennis Schmidt");
+    	Build b2 = new Build(1234568, 2, "red", "Dennis Schmidt");
+    	
+    	initDS(jbs.getJobName(), b1, b2);
+    	
+    	assertTrue(jbs.isNew(10));
+    	assertFalse(jbs.isNew(1));
+    	assertFalse(jbs.isNew(0));
+    }
+    @Test
+    public void checkColorTest(){
+    	
+    	Build b1 = new Build(1234567, 1, "blue", "Dennis Schmidt");
+    	Build b2 = new Build(1234568, 2, "red", "Dennis Schmidt");
+    	initDS(jbs.getJobName(), b1, b2);
+    	assertEquals("destroyed", jbs.checkColor());    	
+    }
+    @Test
+    public void checkColorTest2(){
+    	
+    	Build b1 = new Build(1234567, 1, "blue", "Dennis Schmidt");
+    	Build b2 = new Build(1234568, 2, "blue", "Dennis Schmidt");
+    	initDS(jbs.getJobName(), b1, b2);
+    	assertEquals("successful", jbs.checkColor());
+    	
+    }
+    @Test
+    public void checkColorTest3(){
+    	
+    	Build b1 = new Build(1234567, 1, "red", "Dennis Schmidt");
+    	Build b2 = new Build(1234568, 2, "red", "Dennis Schmidt");
+    	initDS(jbs.getJobName(), b1, b2);
+    	assertEquals("destroyed", jbs.checkColor());
+    	
+    }
+    @Test
+    public void checkColorTest4(){
+    	
+    	Build b1 = new Build(1234567, 1, "blue", "Dennis Schmidt");
+    	Build b2 = new Build(1234568, 2, "red", "Dennis Schmidt");
+    	initDS(jbs.getJobName(), b1, b2);
+    	assertEquals("destroyed", jbs.checkColor());
+    	
+    }
+    
    
 }
